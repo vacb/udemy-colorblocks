399d49bcc54735afc630c34ea87a283a72454456
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2022-01-04 17:23:18 +0000

    Add box component with coloured background that changes on click from an array of colours

---
+++ src/Box.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { choice } from "./helpers";
+import "./Box.css";
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ["purple", "magenta", "violet", "pink", "blue"],
+  };
+  constructor(props) {
+    super(props);
+    this.state = { color: choice(this.props.allColors) };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  pickColor() {
+    let newColor;
+    do {
+      newColor = choice(this.props.allColors);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+  handleClick() {
+    this.pickColor();
+  }
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
