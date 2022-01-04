8c4263048084ad41f56ab6af3321e06eb33a09d4
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2022-01-04 17:43:57 +0000

    Expand allColors array and centralise colors state to BoxContainer component

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -5,11 +5,30 @@ import "./BoxContainer.css";
 class BoxContainer extends Component {
   static defaultProps = {
     numBoxes: 18,
+    allColors: [
+      "purple",
+      "blueViolet",
+      "darkOrchid",
+      "aquamarine",
+      "cadetBlue",
+      "cornflowerBlue",
+      "darkSlateBlue",
+      "lightGreen",
+      "mediumSeaGreen",
+      "orange",
+      "orangeRed",
+      "slateBlue",
+      "teal",
+      "yellow",
+      "yellowGreen",
+      "tomato",
+      "maroon",
+    ],
   };
 
   render() {
     const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
-      <Box />
+      <Box colors={this.props.allColors} />
     ));
     return <div className="BoxContainer">{boxes}</div>;
   }
